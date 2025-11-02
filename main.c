1054dd369065b772e99bde1e0eb9583d1aa345f9
Alba <quillhielo@gmail.com>
2025-11-02 21:08:01 +0100

    Refactor and add print functions for philosophers

--- main.c
+++ main.c
@@ -12,84 +12,6 @@
 
 #include "philo.h"
 
-int	is_digit(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str[i] == '\0')
-		return (0);
-	if (str[i] == '+')
-		i++;
-	while(str[i])
-	{
-		if (!(str[i] >= 48 && str[i] <= 57))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	ascii_to_int(const char *str)
-{
-	size_t		i;
-	long		s;
-	long		r;
-	long		d;
-
-	i = 0;
-	s = 1;
-	r = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			s = s * -1;
-		i++;
-	}
-	if (!(str[i] >= 48 && str[i] <= 57))
-		return (0);
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		d = str[i] - 48;
-		r = r * 10 + d;
-		i++;
-	}
-	return ((int)(r * s));
-}
-int	fill_data(t_data *data, char *str, int count)
-{
-	data->must_eat = -1;
-	if (count == 1)
-		data->n_philos = ascii_to_int(str);
-	else if (count == 2)
-		data->time_to_die = ascii_to_int(str);
-	else if (count == 3)
-		data->time_to_eat = ascii_to_int(str);
-	else if (count == 4)
-		data->time_to_sleep = ascii_to_int(str);
-	else if (count == 5)
-		data->must_eat = ascii_to_int(str);
-	return (0);
-}
-int	parser_argv(char **argv, t_data *data)
-{
-	int	i;
-	
-	i = 1;
-	while(argv[i])
-	{
-		if(!is_digit(argv[i]))
-		{
-			//clean_data(data);
-			return (0);
-		}
-		fill_data(data, argv[i], i);
-		i++;
-	}
-	return (1);
-}
 void	print_data(t_data *data)
 {
 	printf("number of philosophers = %i \n", data->n_philos);
@@ -98,16 +20,35 @@ void	print_data(t_data *data)
 	printf("time_to_sleep = %i \n", data->time_to_sleep);
 	printf("must_eat = %i \n", data->must_eat);
 }
+void	print_philos(t_philo **philos, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while(i < data->n_philos)
+	{
+		printf("id: %i\n", philos[i]->id);
+		printf("has eaten: %i\n", philos[i]->has_eaten);
+		i++;
+	}
+}
 int	main(int argc, char **argv)
 {
 	t_data	*data;
+	t_philo **philos;
+
 	
 	if (argc != 5 && argc != 6)
 		return (0);
 	data = malloc(sizeof(t_data *));
 	if(!parser_argv(argv, data))
+	{
+		free(data);
 		return (0);
+	}
+	philos = philo_init(data);
 	print_data(data);
+	print_philos(philos, data);
 	return (0);
 
 }
